805fec3dc89b388a5e014ca9cb5ab36c76d018db
Nicolas TROADEC - USEWEB <nicolas.troadec@useweb.com>
2023-04-19 10:49:34 +0200

    update

--- functions.php
+++ functions.php
@@ -48,7 +48,7 @@ function block_callback_child($block)
 // Il faut rajouter le bloc de notre thème enfant dans cette liste 
 function my_plugin_allowed_block_types_child($allowed_block_types_all, $post)
 {
-    return array('core/paragraph', 'acf/block-separateur', 'acf/block-2-colonnes-textevisuel', 'acf/block-2-colonnes-textevisuel-large', 'acf/block-multicolonnes', 'acf/block-2-colonnes', 'acf/block-3-colonnes', 'acf/block-1-colonne', 'acf/block-ancres', 'acf/block-cpt-list-filterable', 'acf/block-liste-de-termes');
+    return array('core/paragraph', 'acf/block-separateur', 'acf/block-2-colonnes-textevisuel', 'acf/block-2-colonnes-textevisuel-large', 'acf/block-multicolonnes', 'acf/block-2-colonnes', 'acf/block-3-colonnes', 'acf/block-1-colonne', 'acf/block-ancres', 'acf/block-cpt-list-filterable', 'acf/block-liste-de-termes', 'acf/block-2-colonnes-superposition');
 }
 add_filter('allowed_block_types_all', 'my_plugin_allowed_block_types_child', 11, 3);
 
