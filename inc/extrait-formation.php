cc1cfea1834047bc68c32f054466e753b173a644
Nicolas TROADEC - USEWEB <nicolas.troadec@useweb.com>
2023-05-03 11:21:30 +0200

    update

--- inc/extrait-formation.php
+++ inc/extrait-formation.php
@@ -15,38 +15,39 @@ $texte_pour_le_champ_duree = get_sub_field('texte_pour_le_champ_duree');
 $texte_pour_le_champ_prochaine_session_sur_lextrait = get_sub_field('texte_pour_le_champ_prochaine_session_sur_lextrait');
 $texte_pour_le_bandeau_nouveau = get_sub_field('texte_pour_le_bandeau_nouveau');
 $texte_pour_le_bouton_en_savoir_plus = get_sub_field('texte_pour_le_bouton_en_savoir_plus');
-$texte_bouton_telechargement  = get_sub_field('texte_bouton_telechargement ');
+$texte_bouton_telechargement = get_sub_field('texte_bouton_telechargement');
 ?>
+<!-- <?php print_r(get_post_taxonomies($post_id)) ?>
+<?php print_r(wp_get_post_terms($post_id)); ?> -->
 
-<div class="term_extrait">
-    <div class="term_extrait_content">
-        <figure>
-            <a href="<?php echo the_permalink(); ?>">
+<?php
+$terms = array();
+foreach (get_post_taxonomies($post_id) as $taxonomy) {
+    foreach (wp_get_post_terms($post_id, $taxonomy) as $term) {
+        $terms[] = $term->name;
+    }
+}
 
-                <?php $thumbnail = get_post_thumbnail_id();
-                $size_thumbnail = "medium";
-                $url_thumbnail = $thumbnail ? wp_get_attachment_image_url($thumbnail, $size_thumbnail) : null;
-                $image_weight = $url_thumbnail ? apply_filters('get_weight_of_img', $url_thumbnail) : '0kb'; ?>
 
-                <div class="poids-image"><span class="poids-image-icone"><?php get_template_part('svg/symbole-feuille-nanosite'); ?></span><span class="poids-image-data"><?= $image_weight ?></span></div>
-
-                <?php if ($premiere_taxonomie_du_bandeau) : ?>
-                    <p class="bandeau premier-bandeau">
-                    
-                    </p>
-                <?php endif; ?>
-                <?php if ($deuxieme_taxonomie_du_bandeau) : ?>
-                        <p class="bandeau deuxieme-bandeau">
-
-                        </p>
-                <?php endif; ?>
-                <?php
-                if ($nouveaute_de_extrait) :  ?>
-                    <p class="nouveaute_bandeau"><?= $texte_pour_le_bandeau_nouveau ?></p>
-                <?php endif; ?>
-                <?php echo wp_get_attachment_image($thumbnail, $size_thumbnail, ""); ?>
-            </a>
-        </figure>
+?>
+<div class="extrait_formation">
+    <div class="extrait_content">
+        <div class="terms">
+        <?php if ($terms[0]) : ?>
+            <p class="term term-1">
+                <?= $terms[0] ?>
+            </p>
+        <?php endif; ?>
+        <?php if ($terms[1]) : ?>
+            <p class="term term-2">
+                <?= $terms[1] ?>
+            </p>
+        <?php endif; ?>
+        <?php
+        if ($nouveaute_de_extrait) :  ?>
+            <p class="nouveaute-formation"><?= $texte_pour_le_bandeau_nouveau ?></p>
+        <?php endif; ?>
+        </div>
         <div class="entry-content">
             <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
 
@@ -54,15 +55,15 @@ $texte_bouton_telechargement  = get_sub_field('texte_bouton_telechargement ');
                 <p><?php echo $description_courte; ?></p>
             <?php endif; ?>
             <?php if ($duree) : ?>
-                <p><?= $texte_pour_le_champ_duree ?> <?= $duree ?></p>
-                <p><?= $texte_pour_le_champ_prochaine_session_sur_lextrait ?> <?= $prochaine_session ?></p>
+                <p><span class="duree"><?= $texte_pour_le_champ_duree ?></span> <?= $duree ?></p>
+                <p><span class="formation"><?= $texte_pour_le_champ_prochaine_session_sur_lextrait ?></span> <?= $prochaine_session ?></p>
             <?php endif; ?>
         </div>
         <div class="cta-container-formation-extrait">
             <?php if ($fichier_a_telecharger) : ?>
-                <p class="cta_btn_lead"><a href="<?= $fichier_a_telecharger ?>" target="_blank"><?= $texte_bouton_telechargement ?></a></p>
+                <p class="cta_btn_lead cta_formation cta_ternaire"><a href="<?= $fichier_a_telecharger ?>"><?= $texte_bouton_telechargement ?></a></p>
             <?php endif; ?>
-            <p class="cta_btn_lead"><a href="<?php the_permalink(); ?>"><?= $texte_pour_le_bouton_en_savoir_plus ?></a></p>
+            <p class="cta_btn_lead cta_primaire"><a href="<?php the_permalink(); ?>"><?= $texte_pour_le_bouton_en_savoir_plus ?></a></p>
         </div>
     </div>
 </div>
