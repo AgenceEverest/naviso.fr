92029212fd52ed824612ae8c1638098be5174ef7
Nicolas TROADEC - USEWEB <nicolas.troadec@useweb.com>
2023-05-05 16:23:39 +0200

    update

--- inc/pre-footer.php
+++ inc/pre-footer.php
@@ -5,60 +5,59 @@
 <?php $masquer_le_logo = get_field('masquer_le_logo', 'option'); ?>
 <?php $texte_inscription_newsletter = get_field('texte_inscription_newsletter', 'option'); ?>
 <?php $texte_titre_coordonnees = get_field('texte_titre_coordonnees', 'option'); ?>
+<?php $logo_bretagne_a_droite_du_footer = get_field('logo_bretagne_a_droite_du_footer', 'option') ?>
 
 <?php if (!$masquer_le_prefooter) : ?>
 	<aside id="aside_prefooter" class="padding_section">
 		<div id="aside_prefooter_wrapper" class="">
 			<div class="aside_prefooter_col prefooter_col_1">
 				<?php if (!$masquer_les_coordonnees) : ?>
-					<?php $coordonnees_footer = get_field('coordonnees_footer', 'option'); ?>
-					<h3><?php if ($coordonnees_footer) : ?><?php echo $coordonnees_footer; ?><?php endif; ?></h3>
 					<?php $importer_le_logo_footer = get_field('importer_le_logo_footer', 'option'); ?>
 					<?php $dimensions_logo_prefooter = get_field('dimensions_logo_prefooter', 'option'); ?>
 					<figure id="logo_footer" class="<?php echo $dimensions_logo_prefooter; ?>">
 						<img src="<?php echo $importer_le_logo_footer; ?>" alt="Logo <?php bloginfo('name'); ?>" title="<?php bloginfo('name'); ?>">
 					</figure>
 					<?php $accroche_sous_le_logo_footer = get_field('accroche_sous_le_logo_footer', 'option'); ?>
-						<?php if ($accroche_sous_le_logo_footer) : ?>
-							<p id="aside_prefooter_baseline"><?php echo $accroche_sous_le_logo_footer; ?></p>
+					<?php if ($accroche_sous_le_logo_footer) : ?>
+						<p id="aside_prefooter_baseline"><?php echo $accroche_sous_le_logo_footer; ?></p>
+					<?php endif; ?>
+					<h3 class="titre-coordonnees"><?= $texte_titre_coordonnees ?><h3>
+							<?php $nom = get_field('nom', 'option'); ?>
+							<?php $adresseligne1 = get_field('adresse_ligne_1', 'option'); ?>
+							<?php $adresseligne2 = get_field('adresse_ligne_2', 'option'); ?>
+							<?php $adresseligne3 = get_field('adresse_ligne_3', 'option'); ?>
+							<?php $numerodetelephone = get_field('numero_de_telephone', 'option'); ?>
+							<?php $email = get_field('email', 'option'); ?>
+							<?php $horaires = get_field('horaires', 'option'); ?>
+
+							<?php if ($adresseligne1 || $adresseligne2 || $adresseligne3) : ?>
+								<p class="coordonnees_p_i legende">
+									<?= apply_filters('add_svg', 'map-marker') ?>
+									<?php if ($adresseligne1) : ?>
+										<?php echo $adresseligne1 ?><br>
+									<?php endif; ?>
+									<?php if ($adresseligne2) : ?>
+										<?php echo $adresseligne2 ?><br>
+									<?php endif; ?>
+									<?php if ($adresseligne3) : ?>
+										<?php echo $adresseligne3 ?>
+									<?php endif; ?>
+								</p>
+							<?php endif; ?>
+							<?php if ($numerodetelephone) : ?>
+								<p class="coordonnees_p_i legende">
+									<?= apply_filters('add_svg', 'phone') ?>
+									<?php echo $numerodetelephone ?>
+								</p>
+							<?php endif; ?>
+
+							<?php if ($horaires) : ?>
+								<p class="coordonnees_p_i legende">
+									<?= apply_filters('add_svg', 'clock') ?>
+									<?php echo $horaires ?>
+								</p>
+							<?php endif; ?>
 						<?php endif; ?>
-						<h3><?= $texte_titre_coordonnees ?><h3>
-					<?php $nom = get_field('nom', 'option'); ?>
-					<?php $adresseligne1 = get_field('adresse_ligne_1', 'option'); ?>
-					<?php $adresseligne2 = get_field('adresse_ligne_2', 'option'); ?>
-					<?php $adresseligne3 = get_field('adresse_ligne_3', 'option'); ?>
-					<?php $numerodetelephone = get_field('numero_de_telephone', 'option'); ?>
-					<?php $email = get_field('email', 'option'); ?>
-					<?php $horaires = get_field('horaires', 'option'); ?>
-
-					<?php if ($adresseligne1 || $adresseligne2 || $adresseligne3) : ?>
-						<p class="coordonnees_p_i legende">
-							<?= apply_filters('add_svg', 'map-marker') ?>
-							<?php if ($adresseligne1) : ?>
-								<?php echo $adresseligne1 ?><br>
-							<?php endif; ?>
-							<?php if ($adresseligne2) : ?>
-								<?php echo $adresseligne2 ?><br>
-							<?php endif; ?>
-							<?php if ($adresseligne3) : ?>
-								<?php echo $adresseligne3 ?>
-							<?php endif; ?>
-						</p>
-					<?php endif; ?>
-					<?php if ($numerodetelephone) : ?>
-						<p class="coordonnees_p_i legende">
-							<?= apply_filters('add_svg', 'phone') ?>
-							<?php echo $numerodetelephone ?>
-						</p>
-					<?php endif; ?>
-
-					<?php if ($horaires) : ?>
-						<p class="coordonnees_p_i legende">
-							<?= apply_filters('add_svg', 'clock') ?>
-							<?php echo $horaires ?>
-						</p>
-					<?php endif; ?>
-				<?php endif; ?>
 			</div>
 			<div class="aside_prefooter_col prefooter_col_2">
 				<p class="cta_btn_lead cta_ternaire"><a><?= $texte_inscription_newsletter ?></a></p>
@@ -116,7 +115,7 @@
 					<?php else : endif; ?>
 				<?php endif; ?>
 			</div>
-
+			<img class="logo-bzh-footer" src="<?= $logo_bretagne_a_droite_du_footer ?>">
 		</div>
 		<div class="remove_margin"></div>
 	</aside>
