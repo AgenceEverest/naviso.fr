bff549953e43feb3f2aade34bba38e37d6cbfd69
Nicolas TROADEC - USEWEB <nicolas.troadec@useweb.com>
2023-05-15 17:06:17 +0200

    update

--- inc/extrait-metier.php
+++ inc/extrait-metier.php
@@ -1,7 +1,7 @@
 <?php $image_alt = get_post_thumbnail_id($post->ID)  ? get_post_meta(get_post_thumbnail_id($post->ID), '_wp_attachment_image_alt', TRUE) : null; ?>
 <?php $couleur_lie_au_metier = get_field('choix_de_couleur_pour_le_metier', $post->ID); ?>
 <div class="offre_extrait extrait_metier" style="border-bottom: 6px solid <?= $couleur_lie_au_metier ?>">
-	<div>
+	<div class="extrait-metier-container">
 		<h3>
 			<a href="<?php the_permalink(); ?>">
 				<?php the_title(); ?>
@@ -11,10 +11,12 @@
 		?>
 		<p class="desc"><?= $descriptionextraitdelapage ?></p>
 		<?php $call_to_action_sur_les_extraits_des_metiers = get_field('call_to_action_sur_les_extraits_des_metiers', 'option'); ?>
-		<p class="cta_btn_lead cta_ternaire">
-			<a href="<?php the_permalink(); ?>">
-				<?= $call_to_action_sur_les_extraits_des_metiers; ?>
-			</a>
-		</p>
+		<div class="cta-extrait-container">
+			<p class="cta_btn_lead cta_ternaire">
+				<a href="<?php the_permalink(); ?>">
+					<?= $call_to_action_sur_les_extraits_des_metiers; ?>
+				</a>
+			</p>
+		</div>
 	</div>
 </div>
