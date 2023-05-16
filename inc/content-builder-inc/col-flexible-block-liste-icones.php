9cfa890bc147d0254d7bee10e400a1eb1c191e33
Nicolas TROADEC - USEWEB <nicolas.troadec@useweb.com>
2023-05-16 16:59:26 +0200

    update

--- inc/content-builder-inc/col-flexible-block-liste-icones.php
+++ inc/content-builder-inc/col-flexible-block-liste-icones.php
@@ -1,22 +1,21 @@
 <?php $liste_icones = get_sub_field('liste_icones'); ?>
 <div class="col_flexible_item">
 	<div>
-	    <?php if( have_rows('liste_icones') ):?>
-			<?php while ( have_rows('liste_icones') ) : the_row(); ?>
-				
+		<?php if (have_rows('liste_icones')) : ?>
+			<?php while (have_rows('liste_icones')) : the_row();
+				$icone_texte = get_sub_field('icone_texte');
+				$icone_svg = get_sub_field('icone_svg');
+			?>
+
+				?>
+
 				<div class="element_liste_icones entry-content">
 					<div class="liste_icone_svg">
 						<div class="liste_icone_svg_wrapper">
-							<?php 
-								$icone_svg = get_sub_field('icone_svg'); 
-								$urltheme = get_stylesheet_directory_uri();
-								$pathToSVG = $urltheme . '/img/icons/' .$icone_svg. '.svg';
-								$iconeSVG = file_get_contents($pathToSVG); 
-								echo $iconeSVG; ?>
+							<img src="<?= $iconeSVG; ?>">
 						</div>
 					</div>
 					<div class="liste_icone_txt entry-content">
-						<?php $icone_texte = get_sub_field('icone_texte'); ?>
 						<?php echo $icone_texte; ?>
 					</div>
 				</div>
