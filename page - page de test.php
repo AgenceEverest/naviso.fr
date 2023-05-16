afa3ef9b655dedc0a3f13e74b21d4427a41d60fc
Nicolas TROADEC - USEWEB <nicolas.troadec@useweb.com>
2023-05-16 17:30:06 +0200

    update

--- page - page de test.php
+++ page - page de test.php
@@ -3,9 +3,20 @@
 /**
  * Template Name: Page de test
  */
-
+/* 
  $theme = wp_get_theme();
  $chemin_dossier = $theme->get_stylesheet_directory() . '/img/icons';
  $fichiers = glob($chemin_dossier . '/*');
+
+ foreach ($fichiers as $fichier) {
+    print_r($fichier);
+ }
+
+ print_r($_SERVER);
+
+$chemin_fichier = 'D:\wamp64\www\naviso/wp-content/themes/nanosite-proefficace-child/img/icons/Animaux.svg';
+$repertoire_racine = 'D:\wamp64\www\naviso'; // Chemin du répertoire racine de votre serveur local
+
+$url_fichier = str_replace($repertoire_racine, 'http://localhost/naviso', $chemin_fichier);
+ */
  
- print_r($fichiers);
