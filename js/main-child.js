d37c57d11db3d4f4ec1382967e74f480f634b3bf
Nicolas TROADEC - USEWEB <nicolas.troadec@useweb.com>
2023-05-25 12:32:50 +0200

    update

--- js/main-child.js
+++ js/main-child.js
@@ -1,10 +1,18 @@
 let boutonNewsletter = document.querySelector('#bouton-newsletter');
 let newsletter = document.querySelector('#newsletter');
+let closeButton = document.querySelector('#close-button');
+if(boutonNewsletter) {
+    boutonNewsletter.addEventListener('click', () => {
+        if(newsletter.style.display == 'block') {
+            newsletter.style.display = 'none';
+        } else {
+            newsletter.style.display = 'flex';
+        }
+    })
+}
 
-boutonNewsletter.addEventListener('click', () => {
-    if(newsletter.style.display == 'block') {
-        newsletter.style.display = 'none';
-    } else {
-        newsletter.style.display = 'flex';
-    }
-})
+if (closeButton) {
+    closeButton.addEventListener('click', () => {
+            newsletter.style.display = 'none';      
+    })
+}
