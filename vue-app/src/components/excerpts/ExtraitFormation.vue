a88be529dcc9477c01bb5e55c87ea42d8c36f52e
Nicolas TROADEC - USEWEB <nicolas.troadec@useweb.com>
2023-05-31 14:49:27 +0200

    update

--- vue-app/src/components/excerpts/ExtraitFormation.vue
+++ vue-app/src/components/excerpts/ExtraitFormation.vue
@@ -24,11 +24,11 @@ export default {
       type: String,
     },
     texteCardChampDuree: {
-      type: String, 
+      type: String,
     },
     textCardChampProchaineSession: {
       type: String,
-    }
+    },
   },
   methods: {
     convertToFrenchDate(dateString) {
@@ -42,7 +42,7 @@ export default {
         return new Intl.DateTimeFormat("fr-FR", options).format(date);
       }
     },
-/*     taxoIsShowable(taxo) {
+    /*     taxoIsShowable(taxo) {
       if (taxo[0]) {
         return this.taxonomiesToShow.find((t) => t == taxo[0].taxonomy);
       }
@@ -80,26 +80,15 @@ export default {
       {{ cpt.acf.description_extrait_de_la_page }}
     </p>
     <p v-if="cpt.acf.hasOwnProperty('prochaine_session')">
-      {{ textCardChampProchaineSession }} : 
+      {{ textCardChampProchaineSession }} :
       {{ cpt.acf.prochaine_session }}
-
     </p>
     <p v-if="cpt.acf.hasOwnProperty('duree')">
-      {{ texteCardChampDuree }} : 
+      {{ texteCardChampDuree }} :
       {{ cpt.acf.duree }}
     </p>
 
     <div class="buttons-extrait">
-      <p
-        v-if="
-          cpt.acf.lien_bouton_en_savoir_plus != '' ||
-          cpt.acf.url_pour_le_lien_en_savoir_plus != ''
-        "
-        class="cta_btn_lead cta_primaire"
-        :class="{ cta_center: !afficherBoutonFicheDePoste }"
-      >
-        <a :href="cpt.link">{{ texteEnSavoirPlus }}</a>
-      </p>
       <p
         v-if="
           afficherBoutonFicheDePoste &&
@@ -109,10 +98,20 @@ export default {
       >
         <a
           target="_blank"
-          v-if="cpt.acf.hasOwnProperty('lien_vers_la_fiche_de_poste')"
-          :href="cpt.acf.lien_vers_la_fiche_de_poste"
+          v-if="cpt.acf.hasOwnProperty('fichier_a_telecharger')"
+          :href="cpt.acf.fichier_a_telecharger"
           >{{ texteBoutonFicheFormation }}</a
         >
+      </p>
+      <p
+        v-if="
+          cpt.acf.lien_bouton_en_savoir_plus != '' ||
+          cpt.acf.url_pour_le_lien_en_savoir_plus != ''
+        "
+        class="cta_btn_lead cta_primaire"
+        :class="{ cta_center: !afficherBoutonFicheDePoste }"
+      >
+        <a :href="cpt.link">{{ texteEnSavoirPlus }}</a>
       </p>
     </div>
   </div>
