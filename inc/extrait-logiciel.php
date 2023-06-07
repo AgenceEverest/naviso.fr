1d2d5b52ad8485ce3d240b965091c8c27bb27af0
Nicolas TROADEC - USEWEB <nicolas.troadec@useweb.com>
2023-06-07 17:14:12 +0200

    update

--- inc/extrait-logiciel.php
+++ inc/extrait-logiciel.php
@@ -9,26 +9,39 @@ $texte_pour_le_bouton_en_savoir_plus = get_sub_field('texte_pour_le_bouton_en_sa
 ?>
 
 <?php
-$terms = array();
+/* $terms = array();
 foreach (get_post_taxonomies($post_id) as $taxonomy) {
     foreach (wp_get_post_terms($post_id, $taxonomy) as $term) {
         $terms[] = $term->name;
     }
-}
+} */
 
 ?>
 <div class="cpt-extrait">
     <div class="terms extrait-defaut">
-        <?php if (isset($terms[0])) : ?>
-            <span class="term taxo-0">
-                <?= $terms[0] ?>
-            </span>
-        <?php endif; ?>
-        <?php if (isset($terms[1])) : ?>
-            <span class="term taxo-1">
-                <?= $terms[1] ?>
-            </span>
-        <?php endif; ?>
+        <?php
+        $post_id = get_the_id();
+        $taxonomies = get_post_taxonomies($post_id);
+        $index = 0;
+        foreach ($taxonomies as $taxonomy) {
+            if ($taxonomy !== 'tri_pour_page_formation') {
+
+                $terms = get_the_terms($post_id, $taxonomy);
+                if (is_array($terms) || is_object($terms)) {
+                    if (count($terms) > 0) {
+                        echo '<div class="taxonomy ' . $taxonomy . '">';
+                        if ($terms && !is_wp_error($terms)) {
+                            foreach ($terms as $term) {
+                                echo '<p class="term term-' . $index . '">' . $term->name . '</p>';
+                                $index++;
+                            }
+                        }
+                        echo '</div>';
+                    }
+                }
+            }
+        }
+        ?>
     </div>
     <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
 
