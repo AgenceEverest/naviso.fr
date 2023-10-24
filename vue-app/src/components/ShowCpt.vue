5bef81ad63f3796d13270984cf90db60c64dbf77
Nicolas TROADEC - USEWEB <nicolas.troadec@useweb.com>
2023-10-24 12:21:47 +0200

    update

--- vue-app/src/components/ShowCpt.vue
+++ vue-app/src/components/ShowCpt.vue
@@ -48,9 +48,10 @@ export default {
       }
     }
     this.dataJson = JSON.parse(this.app.getAttribute("data-json"));
+
+    console.log(this.dataJson);
     this.setMaxDisplayablePosts(this.dataJson.max_posts);
     this.setIncrementNumber(this.dataJson.increment_number);
-    console.log(this.dataJson);
     this.getCpt(this.dataJson.publication_liste_app_child).then(() => {
       this.activeAllAtStart();
     });
