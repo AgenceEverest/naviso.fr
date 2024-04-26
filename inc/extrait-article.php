eef8e6de59af7a86e3a90f931d645ea8a16251a2
Agence Everest <contact@agence-everest.com>
2024-04-26 17:20:00 +0200

    new article-extrait

--- inc/extrait-article.php
+++ inc/extrait-article.php
@@ -4,7 +4,6 @@
 		$image_weight = $image_url ? apply_filters('get_weight_of_img', $image_url) : '0kb';
 		$thumbnail_id = get_post_thumbnail_id(get_the_ID());
 		$thumbnail_alt = get_post_meta($thumbnail_id, '_wp_attachment_image_alt', true);
-		$desc = get_field('description_extrait_de_la_page', get_the_ID());
 		?>
 		<a href="<?php the_permalink(); ?>" class="image_article_wrapper">
 			<figure>
@@ -17,37 +16,40 @@
 	</div>
 	<div class="article_extrait_wrapper_text">
 		<p class="entry_title_date legende">
-			<?php $currentlang = get_bloginfo('language'); ?>
-			<?php if ($currentlang == "en-GB") : ?>
-				<span class="date_jour"><?php the_time('M') ?></span><span class="date_mois"><?php the_time('d') ?></span><span class="date_annee"><?php the_time('Y') ?></span>
-			<?php else : ?>
-				<span class="date_jour"><?php the_time('d') ?></span><span class="date_mois"><?php the_time('M') ?></span><span class="date_annee"><?php the_time('Y') ?></span>
-			<?php endif; ?>
-			<?php $domaine_terms = get_the_terms(get_the_ID(), 'category');
-			if (
-				$domaine_terms
-				&& !is_wp_error($domaine_terms)
-			) {
-				@usort($domaine_terms, function ($a, $b) {
-					return strcasecmp(
-						$a->slug,
-						$b->slug
-					);
-				});
-			}
+			<?php 
+				$domaine_terms = get_the_terms(get_the_ID(), 'category');
+				if (
+					$domaine_terms
+					&& !is_wp_error($domaine_terms)
+				) {
+					@usort($domaine_terms, function ($a, $b) {
+						return strcasecmp(
+							$a->slug,
+							$b->slug
+						);
+					});
+					$term_list = [];
+					foreach ($domaine_terms as $term)
+						$term_list[] = '<a href="' . get_term_link($term) . '" class="term_link">' . esc_html($term->name) . '</a>';
+					echo implode(', ', $term_list);
+				}
 			?>
 		</p>
 		<h2 class="article_extrait_post_title">
-			<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+			<a href="<?php the_permalink(); ?>">
 				<?php the_title(); ?>
 			</a>
 		</h2>
-		<?php if ($desc) : ?>
-			<p>
-				<?= $desc ?>
-			</p>
-		<?php endif; ?>
+        <?php $description_extrait_de_la_page = get_field("description_extrait_de_la_page"); 
+            if ($description_extrait_de_la_page){
+                echo "<p class='article_extrait_post_desc'>" . $description_extrait_de_la_page . "</p>";
+            }
+            ?>
 	</div>
-	<?php $calltoactiondesextraitsdarticles = get_field('call-to-action_des_extraits_darticles', 'option'); ?>
-	<a aria-hidden="true" href="<?php the_permalink(); ?>" title="<?php the_title(); ?>" class="btn btn_article_extrait"><?php echo ($calltoactiondesextraitsdarticles); ?></a>
+    <?php $calltoactiondesextraitsdarticles = get_field('call-to-action_des_extraits_darticles', 'option'); ?>
+    <p class="cta_btn_lead cta_primaire cta_extrait_article">
+        <a aria-hidden="true" href="<?php the_permalink(); ?>">
+            <?php echo $calltoactiondesextraitsdarticles; ?>
+        </a>
+    </p>
 </article>
