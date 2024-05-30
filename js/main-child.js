a7a8372e9371814e68c07fb47a55240e5726e468
Agence Everest <contact@agence-everest.com>
2024-05-30 14:13:55 +0200

    -

--- js/main-child.js
+++ js/main-child.js
@@ -104,7 +104,7 @@ function reorderTarteaucitronButtons() {
 }
 document.addEventListener('DOMContentLoaded', function() {
     setTimeout(() => {
-        container.style.display = "block";
+        document.getElementById("tarteaucitronAlertBig").style.display = "block";
         reorderTarteaucitronButtons();
     }, "800");
 });
