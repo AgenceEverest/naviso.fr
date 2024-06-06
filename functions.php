b5cd5508f3bd773d8f5722b3f249dd16d98a0109
Agence Everest <contact@agence-everest.com>
2024-06-06 11:39:22 +0200

    remove exclude post types from Yoast sitemap

--- functions.php
+++ functions.php
@@ -256,17 +256,17 @@ function exclude_taxonomy_from_sitemap( $value, $taxonomy ) {
     return $value;
 }
 
-add_filter( 'wpseo_sitemap_exclude_post_type', 'exclude_posttype_from_sitemap', 10, 2 );
-function exclude_posttype_from_sitemap( $value, $taxonomy ) {
-    if ($post_type == 'metier') {
-        return true;
-    }
-    if ($post_type == 'client') {
-        return true;
-    }
-    return $value;
-}
-add_filter( 'wpseo_exclude_from_sitemap_by_post_ids', 'exclude_from_sitemap_by_id', 10, 2 );
+// add_filter( 'wpseo_sitemap_exclude_post_type', 'exclude_posttype_from_sitemap', 10, 2 );
+// function exclude_posttype_from_sitemap( $value, $taxonomy ) {
+//     if ($post_type == 'metier') {
+//         return true;
+//     }
+//     if ($post_type == 'client') {
+//         return true;
+//     }
+//     return $value;
+// }
+// add_filter( 'wpseo_exclude_from_sitemap_by_post_ids', 'exclude_from_sitemap_by_id', 10, 2 );
 function exclude_from_sitemap_by_id($excluded_posts) {
     $excluded_posts[] = 65; // C'est l'ID de la page contact
     //$excluded_posts[] = 124; // Ajoutez autant de lignes que nécessaire pour exclure d'autres pages ou articles par leur ID.
