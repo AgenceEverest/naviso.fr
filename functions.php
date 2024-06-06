d8bdd1630793f3af3eb467d2e8a12ca65ebab0c0
Agence Everest <contact@agence-everest.com>
2024-06-06 11:50:04 +0200

    defer attribute on main-child.js import

--- functions.php
+++ functions.php
@@ -7,13 +7,29 @@ function styles2_css()
         get_stylesheet_uri(),
         array('parenthandle')
     );
+    wp_enqueue_script('main-child', get_stylesheet_directory_uri() . '/js/main-child.js', array(), filemtime(get_stylesheet_directory() . '/js/main-child.js'), true);
+
     // Chargement de mainchild.css pour le thème enfant.
     wp_enqueue_style('mainchild', get_stylesheet_directory_uri() . '/css/mainchild.css', array(), filemtime(get_stylesheet_directory() . '/css/mainchild.css'), false);
-    wp_enqueue_script('mainchild', get_stylesheet_directory_uri() . '/js/main-child.js', array(), filemtime(get_stylesheet_directory() . '/js/main-child.js'), true);
 }
 
 //la priorité à 99, c'est pour que main2.css soit chargé après main.css, de cette manière les règles CSS du thème enfant peuvent écraser celles du thème parent.
 add_action('wp_enqueue_scripts', 'styles2_css', 99);
+
+
+
+// Add a defer attribute to main-child script
+function add_defer_attribute_to_script_child($tag, $handle, $src) {
+    // Vérifie si le handle du script est celui que vous voulez modifier
+    if ('main-child' === $handle) {
+        // Ajoute l'attribut defer à la balise script
+        $tag = '<script src="' . esc_url($src) . '" defer="defer"></script>';
+    }
+    return $tag;
+}
+add_filter('script_loader_tag', 'add_defer_attribute_to_script_child', 10, 3);
+
+
 
 // Désenregistrer ArkFilter
 function arkfilter_deregister_scripts() {
